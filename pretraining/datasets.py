1826a7c707060474ccae545c3834cb589f29fc47
Nuren <zh.nuren@gmail.com>
2024-01-15 16:27:04 +0400

    adding isic2020

--- pretraining/datasets.py
+++ pretraining/datasets.py
@@ -138,6 +138,62 @@ class Transforms:
             # combined_train_transform = v2.Compose([lambda img: train_transform(image=np.array(img))['image'], tensor_transform])
             # combined_val_transform = v2.Compose([lambda img: val_transform(image=np.array(img))['image'], tensor_transform])
 
+    class ISIC2020:
+        class ImageNet:
+            train_transform = v2.Compose([
+                v2.RandomResizedCrop((224, 224), scale=(0.9, 1.0), antialias=True),
+                v2.ToTensor(),
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+            val_transform = v2.Compose([
+                v2.Resize((224, 224), antialias=True),
+                v2.ToTensor(),
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ])
+
+        class Random:
+            train_transform = A.Compose([
+                A.Transpose(p=0.5),
+                A.VerticalFlip(p=0.5),
+                A.HorizontalFlip(p=0.5),
+                A.RandomBrightness(limit=0.2, p=0.75),
+                A.RandomContrast(limit=0.2, p=0.75),
+                A.OneOf([
+                    A.MotionBlur(blur_limit=5),
+                    A.MedianBlur(blur_limit=5),
+                    A.GaussianBlur(blur_limit=5),
+                    A.GaussNoise(var_limit=(5.0, 30.0)),
+                ], p=0.7),
+
+                A.OneOf([
+                    A.OpticalDistortion(distort_limit=1.0),
+                    A.GridDistortion(num_steps=5, distort_limit=1.),
+                    A.ElasticTransform(alpha=3),
+                ], p=0.7),
+
+                A.CLAHE(clip_limit=4.0, p=0.7),
+                A.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=20, val_shift_limit=10, p=0.5),
+                A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, border_mode=0, p=0.85),
+                A.Resize(224, 224),
+                A.Cutout(max_h_size=int(224 * 0.375), max_w_size=int(224 * 0.375), num_holes=1, p=0.7),
+            ])
+
+
+
+
+
+            val_transform = A.Compose([
+                A.Resize(224, 224),
+            ])
+
+            tensor_transform = v2.Compose([
+                v2.ToTensor(),
+                v2.Normalize(mean=[0, 0, 0], std=[1, 1, 1])
+            ])
+
+
+
+
 
 def get_dataset(DATASET, paths, PRETRAINING, IMAGE_SIZE, BATCH_SIZE, NUM_WORKERS, TASK):
     
@@ -184,6 +240,10 @@ def get_dataset(DATASET, paths, PRETRAINING, IMAGE_SIZE, BATCH_SIZE, NUM_WORKERS
         train_dataset = AptosDataset(csv_file=paths[1], data_folder=paths[0], split = 'train', pretraining = PRETRAINING, task = TASK, transform=transforms.train_transform, post_transform = transforms.tensor_transform)
         val_dataset = AptosDataset(csv_file=paths[1], data_folder=paths[0], split = 'val', pretraining = PRETRAINING, task = TASK, transform=transforms.val_transform, post_transform = transforms.tensor_transform)
         test_dataset = AptosDataset(csv_file=paths[1], data_folder=paths[0], split = 'test', pretraining = PRETRAINING, task = TASK, transform=transforms.val_transform, post_transform = transforms.tensor_transform)
+    elif DATASET == "ISIC2020":
+        train_dataset = ISIC2020Dataset(csv_file=paths[1], data_folder=paths[0], split = 'train',  transform=transforms.train_transform, post_transform = transforms.tensor_transform)
+        val_dataset = ISIC2020Dataset(csv_file=paths[1], data_folder=paths[0], split = 'val', transform=transforms.val_transform, post_transform = transforms.tensor_transform)
+        test_dataset = ISIC2020Dataset(csv_file=paths[1], data_folder=paths[0], split = 'val', transform=transforms.val_transform, post_transform = transforms.tensor_transform)
 
 
     print(DATASET, len(train_dataset), len(val_dataset), len(test_dataset))
@@ -247,4 +307,27 @@ class AptosDataset(Dataset):
             image = self.transform(image = image)['image']
             image = self.post_transform(image)
         
+        return image, label
+
+
+class ISIC2020Dataset(Dataset):
+    def __init__(self, csv_file, data_folder, split, transform=None, post_transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.root_dir = data_folder
+        self.split = split
+        self.data = self.data.loc[self.data["split"] == self.split]
+        self.transform = transform
+        self.post_transform = post_transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, str(self.data.iloc[idx, 1]), str(self.data.iloc[idx, 0]))
+        image = np.array(Image.open(img_name))
+        label = self.data.iloc[idx, 1]
+
+        if self.transform:
+            image = self.transform(image = image)['image']
+            image = self.post_transform(image)
         return image, label
